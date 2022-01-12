88af4279b74be9a9c7f514fe46f07053be7ee94f
PWaclawczyk <pwaclawczyk@carbo.com.pl>
2022-01-12 15:04:54 +0100

    Buttons' labels changed Added registry script

--- ClipboardBase64Converter/MainForm.Designer.cs
+++ ClipboardBase64Converter/MainForm.Designer.cs
@@ -40,7 +40,7 @@ namespace ClipboardBase64Converter
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 86);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Otwórz plik i skopiuj do schowka jako Base64";
+            this.button1.Text = "Open a file and copy it to clipboard as Base64";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -50,7 +50,7 @@ namespace ClipboardBase64Converter
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(91, 86);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Zapisz Base64 ze schowka jako plik";
+            this.button2.Text = "Save Base64 content in clipboard a a file";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
